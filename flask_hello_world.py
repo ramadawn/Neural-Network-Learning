0a9b718fd58c512259abffd0bbf3a0e32f984347
Douglas Oak <ramadawn@gmail.com>
2017-10-05 11:53:59 -0700

    Code: Mnist CNN flask interactive code

---
+++ flask_hello_world.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+#Initialize the app from Flask
+app = Flask(__name__)
+
+#Define a route to hello_world function
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+#Run the app on http://localhost:8085
+app.run(debug=True,port=3085)
